fce8051f098d326b8833902c5c407f055b044c76
Bartek Antos <bartekantos@protonmail.com>
2022-07-05 21:02:58 +0200

    Added new Login Window prototype, Created DataContext file.

--- CRM/DataContext.cs
+++ CRM/DataContext.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClassSchedule
 {
-    internal class DataContext
+    public class DataContext: DbContext
     {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = database.db");
+        }
+
+
+
+        public DbSet<User> Users { get; set; }
+
+
     }
+
 }
--- CRM/User.cs
+++ CRM/User.cs
@@ -2,5 +2,10 @@
 {
     public class User
     {
+        public int ID { get; set; } 
+        
+        public string Username { get; set; }
+
+        public string Password { get; set; }
     }
 }
