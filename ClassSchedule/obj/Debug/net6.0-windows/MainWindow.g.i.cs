95e0bf13677b1b79e515cb98f7a5f1098eb51dbc
Bartek Antos <bartekantos@protonmail.com>
2022-07-08 22:50:23 +0200

    Created app installer. Fixed Main Window UI bugs.

--- ClassSchedule/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ ClassSchedule/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -149,7 +149,7 @@ namespace ClassSchedule {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ClassSchedule;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ClassSchedule;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
